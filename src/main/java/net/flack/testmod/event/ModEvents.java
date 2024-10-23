718df9a4e7f356c1731e2d1f5871f933a2f4bd06
FlackCode <fl4ckc0de@gmail.com>
2024-10-23 21:24:52 +0300

    Added a Cool Custom Event

--- src/main/java/net/flack/testmod/event/ModEvents.java
+++ src/main/java/net/flack/testmod/event/ModEvents.java
@@ -3,11 +3,17 @@ package net.flack.testmod.event;
 import net.flack.testmod.TestMod;
 import net.flack.testmod.item.custom.HammerItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 import java.util.HashSet;
@@ -36,6 +42,16 @@ public class ModEvents {
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent.Pre event) {
+        if (event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player) {
+            if (player.getMainHandItem().getItem() == Items.END_ROD) {
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a sheep with an End Rod."));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 6));
+                player.getMainHandItem().shrink(1);
             }
         }
     }
